4089bc147d31b02e20aebca26a88d81d40d050bb
elissandroa <elissandroa@hotmail.com>
2025-04-27 13:25:48 -0300

    Starting project

---
+++ backend/src/main/java/com/elissandro/hdcontrol/HdcontrolApplication.java
@@ -0,0 +1,13 @@
+package com.elissandro.hdcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdcontrolApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/elissandro/hdcontrol/HdcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elissandro.hdcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
