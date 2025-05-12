e72e4244a78db6a5983fc87a699a2990a5e985bb
elissandroa <elissandroa@hotmail.com>
2025-05-12 10:12:15 -0300

    Custom ConstraintValidator

--- backend/src/main/java/com/elissandro/hdcontrol/dtos/UserInsertDTO.java
+++ backend/src/main/java/com/elissandro/hdcontrol/dtos/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.elissandro.hdcontrol.dtos;
 
+import com.elissandro.hdcontrol.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 	private static final long serialVersionUID = 1L;
 
--- backend/src/main/java/com/elissandro/hdcontrol/repositories/UserRepository.java
+++ backend/src/main/java/com/elissandro/hdcontrol/repositories/UserRepository.java
@@ -6,4 +6,6 @@ import com.elissandro.hdcontrol.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	User findByEmail(String email);
+
 }
