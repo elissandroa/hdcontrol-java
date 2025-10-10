b4830c3844087c00e2106dc651ba693bf4191f5c
Elissandro Aparecido Anastácio <58570300+elissandroa@users.noreply.github.com>
2025-10-10 08:52:48 -0300

    update 404.html

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,5 +3,15 @@
   import App from "./App.tsx";
   import "./index.css";
 
+  // Recuperar redirecionamento salvo
+const redirect = sessionStorage.getItem('redirect');
+if (redirect) {
+  // Limpa para não causar loops ou reuso indesejado
+  sessionStorage.removeItem('redirect');
+  // Substitui a URL atual pela rota que o usuário queria originalmente
+  window.history.replaceState(null, '', redirect);
+}
+
+
   createRoot(document.getElementById("root")!).render(<App />);
   
